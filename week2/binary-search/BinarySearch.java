aeafcce58c41a917ccdff1584c8a0a7f9909ef10
Ofiyusha07 <ofiya.arabbaeva@alatoo.edu.kg>
2026-09-25 09:05:25 +0600

    Implement binary search algorithm in BinarySearch.java
    Add binary search solution

--- week2/binary-search/BinarySearch.java
+++ week2/binary-search/BinarySearch.java
@@ -1,1 +1,30 @@
+public class BinarySearch {
 
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+
+        System.out.println(search(nums, 9));
+        System.out.println(search(nums, 2));
+    }
+}
